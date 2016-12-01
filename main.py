ce809b0ed5e3276bfad843f797d89d8da5d6989a
Michał Zalewski <michalzalewski0@gmail.com>
2016-12-01 19:30:47 +0100

    kolory xdd

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from graphics import *
 from random import *
 
-
+FLAT_UI_COLORS = [[color_rgb(26, 188, 156), color_rgb(46, 204, 113), color_rgb(52, 152, 219), color_rgb(155, 89, 182)],
+                    [color_rgb(241, 196, 15), color_rgb(230, 126, 34), color_rgb(231, 76, 60), color_rgb(189, 195, 199)]]
 WIN_TITLE = "ConnectCztery"
-COL = 10
+COL = 7
 ROW = 6
 WIN_X = 700
 WIN_Y = 700 * ROW/COL
 RADIUS = 3/4 * WIN_X / (2*COL)
+COLOR_1 = FLAT_UI_COLORS[0][randint(0,3)]
+COLOR_2 = FLAT_UI_COLORS[1][randint(0,3)]
+BG_COLOR = "white"
 
 def draw_grid(win):
     for i in range(COL):
@@ -23,15 +27,16 @@ def draw_circle(win, board, filled, col_num, player):
     filled[col_num] += 1
     c = Circle( Point(WIN_X/(2*COL) + col_num*WIN_X/COL, WIN_Y/(2*ROW) + (ROW - row_num - 1)*WIN_Y/ROW), RADIUS )
     if (player == 1):
-        c.setFill("yellow")
-        c.setOutline("yellow")
+        c.setFill(COLOR_1)
+        c.setOutline(COLOR_1)
     else:
-        c.setFill("red")
-        c.setOutline("red")
+        c.setFill(COLOR_2)
+        c.setOutline(COLOR_2)
     c.draw(win)
 
 def main():
     win = GraphWin(WIN_TITLE, WIN_X, WIN_Y)
+    win.setBackground(BG_COLOR)
     board = []
     filled = []
     for i in range(COL):
