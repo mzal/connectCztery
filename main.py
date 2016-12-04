6ad69aac2d1f7daa464c8855fc89d277d474fb4a
Maciej <darth2011@wp.pl>
2016-12-04 19:03:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,6 @@ from win_check import *
 import copy
 
 
-
-WIN_TITLE = "ConnectCztery"
-COL = 7
-ROW = 6
-WIN_X = 700
-WIN_Y = 700 * ROW/COL
-RADIUS = 3/4 * WIN_X / (2*COL)
-background_color = "white"
-TL = 4
-debug = True
-#TL = Target Length
-
 def draw_grid(win):
     win.setBackground(background_color)
     for i in range(1,COL):
