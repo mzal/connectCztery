9897e86252930c6b28b8d4f8dd83bbe3dac85b50
Maciej <darth2011@wp.pl>
2016-12-10 14:41:48 +0000

    Update evaluate2.py

--- evaluate2.py
+++ evaluate2.py
@@ -1,23 +1,22 @@
 from win_check import *
+from static_values import *
 
-v = [0,1,5,20]
-
-def evaluate2(board,first,second):
+def evaluate2(board, v):
     points = 0
-    if(win_check(board)==second):
-        points=1000000000
+    if(win_check(board)==2):
+        points=9000000000
         return points
-    if(win_check(board)==first):
-        points=-1000000000
+    if(win_check(board)==1):
+        points=-9000000000
         return points
     for i in range(COL):
         for j in range(ROW-TL+1):
             a = 0
             b = 0
             for k in range(TL):
-                if(board[i][j+k]==first):
+                if(board[i][j+k]==1):
                     a+=1
-                if(board[i][j+k]==second):
+                if(board[i][j+k]==2):
                     b+=1
             if(a==0):
                 points+=v[b]
@@ -28,9 +27,9 @@ def evaluate2(board,first,second):
             a = 0
             b = 0
             for k in range(TL):
-                if(board[i+k][j]==first):
+                if(board[i+k][j]==1):
                     a+=1
-                if(board[i+k][j]==second):
+                if(board[i+k][j]==2):
                     b+=1
             if(a==0):
                 points+=v[b]
@@ -41,9 +40,9 @@ def evaluate2(board,first,second):
             a = 0
             b = 0
             for k in range(TL):
-                if(board[i+k][j+k]==first):
+                if(board[i+k][j+k]==1):
                     a+=1
-                if(board[i+k][j+k]==second):
+                if(board[i+k][j+k]==2):
                     b+=1
             if(a==0):
                 points+=v[b]
@@ -52,13 +51,12 @@ def evaluate2(board,first,second):
             a = 0
             b = 0
             for k in range(TL):
-                if(board[i+TL-k-1][j+k]==first):
+                if(board[i+TL-k-1][j+k]==1):
                     a+=1
-                if(board[i+TL-k-1][j+k]==second):
+                if(board[i+TL-k-1][j+k]==2):
                     b+=1
             if(a==0):
                 points+=v[b]
             if(b==0):
-
                 points-=v[a]
-    return points
+    return -points
